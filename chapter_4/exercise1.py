491f740ef32729f668dbc66a4e46685fcd0dafba
Derek2421 <112088724+Derek242@users.noreply.github.com>
2022-09-08 04:05:17 +0000

    Exercises for Chapter 4

---
+++ chapter_4/exercise1.py
@@ -0,0 +1,3 @@
+# Write a program that prints We like Python's turtles! 100 times.
+for i in range(100):
+ print("We like Python's turtles!")
---
+++ chapter_4/exercise3.py
@@ -0,0 +1,4 @@
+# Write a program that uses a for loop to print "One of the months of the year is January, One of the months of the year is February, One of the months of the year is March etc …"
+
+for i in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October' 'November', 'December']:
+  print("One of the months of the year is", i)
