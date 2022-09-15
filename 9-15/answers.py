d8254c59a0f8b39f592b570f9675029e814711db
Derek2421 <112088724+Derek242@users.noreply.github.com>
2022-09-15 03:32:18 +0000

    Update Names

--- 9-15/answers.py
+++ 9-15/answers.py
@@ -18,14 +18,17 @@ def is_odd(n):
 print(is_odd(5))
 print(is_odd(8))
 
-#7.10: Write a function is_rightangled which, given the length of three sides of a triangle, will determine whether the triangle is right-angled. Assume that the third argument to the function is always the longest side. It will return True if the triangle is right-angled, or False otherwise.
+#7.10-11: Write a function is_rightangled which, given the length of three sides of a triangle, will determine whether the triangle is right-angled. Assume that the third argument to the function is always the longest side. It will return True if the triangle is right-angled, or False otherwise.
 
 #Hint: floating point arithmetic is not always exactly accurate, so it is not safe to test floating point numbers for equality. If a good programmer wants to know whether x is equal or close enough to y, they would probably code it up as
+
+
 def is_rightangled(x,y,z):
   xa = x**2
   ya = y**2
   za = z**2
   if(xa+ya==za or ya+za==xa or xa+za==ya):
+    #Extend the above program so that the sides can be given to the function in any order.
     return True
   else:
     return False
