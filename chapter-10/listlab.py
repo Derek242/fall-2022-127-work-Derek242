41ec55459786972c732ae247f6482b64f988b97b
Derek2421 <112088724+Derek242@users.noreply.github.com>
2022-10-13 15:09:21 +0000

    lab

--- chapter-10/listlab.py
+++ chapter-10/listlab.py
@@ -25,18 +25,74 @@ print(odd(l))
 lower = ["derek ni"]
 upper = []
 def caplist(word):
-  
+  nl = ''
+  for i in word.split():
+        upper += i.captalize() + " " # CAPITALIZE EACH WORD AND ADD IT TO NEWLIST
+   return upper
   
 
   
 #Write a function that takes a string and returns a new string with every word that's longer than 5 character turned into upper case
 
+def more_than_five(l):
+    finalList = []
+    for i in l.split():
+        if len(i) > 4: 
+            i = i.upper()
+        newlist.append(i)
+        sentence = " ".join(newlist)
+    return sentence
   
 #Write a function that takes a list of numbers and returns a new list with each item squared
-
+lis = [2,3,4,5]
+def squareRoot(l):
+  squaredList = []
+  for i in l:
+    i = i**2
+    squaredList.append(i)
+  return squaredList
+print(squareRoot(lis))
   
 #Write a function that takes two lists of numbers and returns a new list where each item is the corresponding values of the original lists added together. Ex [1,2,3] and [10,20,30] would return the list [11,22,33]
+lis1 = [1,2,3,4,5]
+lis2 = [6,7,8,9,10]
+def combineList(l1,l2):
+  counter = 0
+  finalList = []
+  for i in lis1:
+      finalList.append(i+l2[counter])
+      counter = counter + 1
+  return finalList
 
-  
-#chapter 10 # 10, 11, 12
+print(combineList(lis1,lis2))
+#chapter 10:
+#10
+lis = ["Banana","Monke", "Billy"]
+def fiveletters(l):
+  finalList = []
+  for i in l:
+    if len(i) >= 5:
+      finalList.append(i)
+  return finalList
+print(fiveletters(lis))
+
+#11
+lis = [1,3,7,9,5,2]
+def sum_of_odds(l):
+  finalList = []
+ for i in l:
+   if num%2 == 0:
+     return finalList
+   else:
+    finalList = finalList + i
+
+print(sum_of_odds(lis))
+
+#12
+def count_sam(l):
+  counter = 0
+for i in l:
+  counter = counter + 1
+  if i == "sam":
+    return l
 
