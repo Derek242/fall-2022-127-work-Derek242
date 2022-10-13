a18febe327c30055cd1f569771be17c3719478e4
Derek2421 <112088724+Derek242@users.noreply.github.com>
2022-10-13 02:59:25 +0000

    lab

---
+++ chapter-10/listlab.py
@@ -0,0 +1,42 @@
+# Write a function to find the smallest value in a listKfind smallest in a list of items
+l = [4,7,10,5]
+
+def smallest(listKfind):
+  small = listKfind[0]
+  for i in (listKfind):
+    if i < small:
+      small = i
+  return small
+  
+print(smallest(l))
+
+#Write a function that returns a new list that contains all the odd items in the original list
+l = [1,5,17,20,567,28,60]
+def odd(l):
+  oddlist = []
+  for i in l:
+    if i%2 == 1:
+      oddlist.append(i)
+  return oddlist
+
+print(odd(l))
+  
+#Write a function that takes a string and returns a new string where all the words are capitalized.
+lower = ["derek ni"]
+upper = []
+def caplist(word):
+  
+  
+
+  
+#Write a function that takes a string and returns a new string with every word that's longer than 5 character turned into upper case
+
+  
+#Write a function that takes a list of numbers and returns a new list with each item squared
+
+  
+#Write a function that takes two lists of numbers and returns a new list where each item is the corresponding values of the original lists added together. Ex [1,2,3] and [10,20,30] would return the list [11,22,33]
+
+  
+#chapter 10 # 10, 11, 12
+
