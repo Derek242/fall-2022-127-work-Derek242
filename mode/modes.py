a0694862e848921c47df00402786e613ff1b3551
Derek242 <derek.ni47@myhunter.cuny.edu>
2022-11-02 16:20:22 -0400

    mode stuff

--- mode/modes.py
+++ mode/modes.py
@@ -17,23 +17,39 @@ lis = [9,69,5,19,1,5,25,69,48]
 def freq(l,v):
   count = 0
   for i in l:
-    if x == v:
+    if i == v:
       count += 1
   return count
 
 print(freq(lis,value))
 
 
-modeSoFar = dataset[0]
-freqSoFar = freq(dataset,modeSoFar)
-for item in dataset[1:]:
-  modeSoFar = item
-  freqSoFar = freq(dataset, item)
-return modeSoFar
+import random
+def fastMode(dataset):
+    # assume all values in dataset
+    # are between 0 and 99 inclusive
+    def buildRandomList(size,maxvalue):
+      #result = []
+     #for x in range(size):
+      #    result.append(random.randrange(maxvalue))
+      #return result
+     result = [random.randrange(maxvalue) for x in range(size)]
+     return result 
 
-def testMode(size, maxV):
-  dataset = builtRandom(size, maxV)
-  print(dataset)
-  m = mode(dataset)
-  print(m)
-  
+
+    # 1. make a list of 100 slots
+    # and set them all to 0
+    # this will store our tallies
+    newL = []
+    tallies = 0
+    
+
+    # 2. Loop through our dataset
+    # and for each item incremement
+    # (add 1) to the appropriate
+    # slot in the tallies list
+
+    # 3. the index with the highest
+    # value in tallies is the mode
+
+    pass
