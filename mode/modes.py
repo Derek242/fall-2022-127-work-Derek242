213097bfe1a8b430757113e4e016721d68bf6ee3
Derek242 <derek.ni47@myhunter.cuny.edu>
2022-11-02 22:55:46 -0400

    fastmode

--- mode/modes.py
+++ mode/modes.py
@@ -23,33 +23,102 @@ def freq(l,v):
 
 print(freq(lis,value))
 
+import datetime
+import random
+
+def findLargest(dataset):
+    largeSoFar = dataset[0]
+    for item in dataset[1:]:
+        if item > largeSoFar:
+            largeSoFar = item
+    return largeSoFar
+
+
+
+def freq(dataset,v):
+    # since this loops over the
+    # entire data set once
+    # it takes n time 
+    #count = 0
+    #for item in dataset:
+    #    if item == v:
+    #        count = count + 1
+    #return count
+    return len([x for x in dataset if x == v])
+
+def buildRandomList(size,maxvalue):
+    #result = []
+    #for x in range(size):
+    #    result.append(random.randrange(maxvalue))
+    #return result
+    result = [random.randrange(maxvalue) for x in range(size)]
+    return result 
+
 
 import random
 def fastMode(dataset):
     # assume all values in dataset
     # are between 0 and 99 inclusive
-    def buildRandomList(size,maxvalue):
-      #result = []
-     #for x in range(size):
-      #    result.append(random.randrange(maxvalue))
-      #return result
-     result = [random.randrange(maxvalue) for x in range(size)]
-     return result 
 
 
     # 1. make a list of 100 slots
     # and set them all to 0
     # this will store our tallies
-    newL = []
-    tallies = 0
+    tallies = [0]*100
     
 
     # 2. Loop through our dataset
     # and for each item incremement
     # (add 1) to the appropriate
     # slot in the tallies list
-
+    for i in tallies:
+      tallies[i] += 1
     # 3. the index with the highest
     # value in tallies is the mode
+    return tallies
 
-    pass
+
+
+def mode(dataset):
+    """
+    Returns a mode of the dataset, that is
+    the value that appears most frequently
+    if multiple values appear the same # of times and are
+    most frequent, return any of them.
+    Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most
+    mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since
+    both of those values appear 3 times which is the most
+    Strategy:
+    assume the first value is the mode
+    we can grab its frequency
+    for each remaining item in the dataset:
+      count that items frequence and see if it's the new
+      mode so far    
+    """
+    modeSoFar = dataset[0]
+    freqSoFar = dataset.count(modeSoFar)
+    for item in dataset[1:]: #outer loop -> n
+        # calling freq each time is n
+        # if freq(dataset,item) > freqSoFar:
+        if dataset.count(item) > freqSoFar:
+            modeSoFar = item
+            freqSoFar = dataset.count(item)
+    return modeSoFar
+
+    
+def testMode(size,maxValue):
+    print("Dataset Size: ",size)
+    dataset = buildRandomList(size,maxValue)
+    # print(dataset)
+    m = mode(dataset)
+    print("Mode: ",m)
+
+def testFindLargest(size,maxValue):
+    print("Dataset Size: ",size)
+    dataset = buildRandomList(size,maxValue)
+    # print(dataset)
+    m = findLargest(dataset)
+    print("Largest: ",m)
+
+#testFindLargest(80000,30)
+testMode(40000,30)
