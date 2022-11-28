2e14ac020bd5553cd0263ae19ec9cfa111b8e1db
Derek242 <derek.ni47@myhunter.cuny.edu>
2022-11-28 06:37:18 -0500

    last touches

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,36 +1,38 @@
 #Extra: 
 # 1)Handle upper and lower case and/or punctuation
 
-
-pirate = open('pirate.dat')
 speech = open('input.txt')
+pirate = open('pirate.dat')
 pirate_speech = pirate.read()
 sentences = speech.read().lower()
-pirate2 = pirate_speech.split('\n')
-speech2 = speech.split('\n')
+pirate2 = pirate_speech.split()
+speech2 = sentences.split()
 
-i = 0
-story =[]
-dictionary = {}
+
+dict_pirate = {}
 for i in speech2:
     semi = i.find(':')
-    dict.update({i[0:semi]:i[semi+1:]})
+    dict_pirate.update({i[0:semi]:i[semi+1:]})
 
-for i in pirate2:
-    if i in dictionary.keys():
-        story.append(dictionary[i])
-    else:
-        story.append(i)
-    if story[i] == story[0]:
-        story[i] = story[i].capitalize()
-    elif "." in story[i-1]:
-        story[i] = story[i].captialize()
-    elif "!" in story[i-1]:
-        story[i] = story[i].captialize()
-    elif "?" in story[i-1]:
-        story[i] = story[i].captialize()
-    elif ";" in story[i-1]:
-        story[i] = story[i].captialize()
+def dictionary(dict):
+    story = []
+    i=0
+    for i in pirate2:
+        if i in dict.keys():
+            story.append(dictionary[i])
+        else:
+            story.append(i)
+        if story[i] == story[0]:
+            story[i] = story[i].capitalize()
+        elif "." in story[i-1]:
+            story[i] = story[i].captialize()
+        elif "!" in story[i-1]:
+            story[i] = story[i].captialize()
+        elif "?" in story[i-1]:
+            story[i] = story[i].captialize()
+        elif ";" in story[i-1]:
+            story[i] = story[i].captialize()
     i+= 1
+    return ' '.join(story)
     
-print(' '.join(story))
+print("Pirate Speech: " + dictionary(dict_pirate))
