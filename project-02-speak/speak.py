b00dd4704deddfdffc10d06538b3891782dec388
Derek242 <derek.ni47@myhunter.cuny.edu>
2022-11-28 01:36:03 -0500

    changes

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,26 +1,26 @@
 #Extra: 
 # 1)Handle upper and lower case and/or punctuation
-# 2)
 
-pirate = open('pirate.dat','r')
-speech = open('input.txt','r')
+
+pirate = open('pirate.dat')
+speech = open('input.txt')
 pirate_speech = pirate.read()
 sentences = speech.read().lower()
-pirate2 = pirate_speech.split()
+pirate2 = pirate_speech.split('\n')
 speech2 = speech.split('\n')
 
 i = 0
 story =[]
 dictionary = {}
 for i in speech2:
-    semi = word.find(':')
-    dict.update({word[0:semi]:word[semi+1:]})
+    semi = i.find(':')
+    dict.update({i[0:semi]:i[semi+1:]})
 
 for i in pirate2:
-    if word in dictionary.keys():
+    if i in dictionary.keys():
         story.append(dictionary[i])
     else:
-        story.append(word)
+        story.append(i)
     if story[i] == story[0]:
         story[i] = story[i].capitalize()
     elif "." in story[i-1]:
